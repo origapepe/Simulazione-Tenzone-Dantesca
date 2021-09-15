593f1586936bd32e0547382d0a2f5b193c98e0b9
origapepe <origapepe@gmail.com>
2021-09-15 20:19:01 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,84 @@
+var arr = [
+[
+"Avvegna che la subitana fuga\n dispergesse color per la campagna,\n rivolti al monte ove ragion ne fruga,\n", //1 giorno
+"i' mi ristrinsi a la fida compagna:\n e come sare'io sanza lui corso?\n chi m'avria tratto su per la montagna?\n",
+"El mi parea di se' stesso rimorso\n o dignitosa coscienza e netta\n come t'e' picciol fallo amaro morso!\n", 
+"Quando li piedi suoi lasciar la fretta,\n che l'onestade ad ogn'atto dismaga,\n la mente mia, che prima era ristretta\n", // 2 giorn1
+"lo'ntento rallargo', si' come vaga,\n e diedi 'l viso mio incontr' al poggio,\n che 'nverso 'l ciel piu' alto si dislaga.\n",
+"Lo sol, che fiammeggiava roggio,\n rotto m'era dinanzi a la figura,\n ch'avea in me de' suoi raggi l'appoggio.\n",
+"Io mi volsi dallato con paura\n d'essere abbandonato, quand'io vidi\n solo dinanzi a me la terra oscura;\n",
+"e 'l mio conforto \"Perche pur diffidi?\n a dir mi comincio' tutto rivlolto.\n \"non credi tu me teco e ch'io ti guidi?\"\n",
+"Vespero e' gia' cola' dov'e' sepolto\n lo corpo dentro al quale io facea ombra\n Napoli l'ha, e da Brandizio e' tolto.\n",
+"Ora, se innanzi a me null s'aombra,\n non ti maravigliar piu' che d'i cieli\n che l'uno a l'altro raggio non ingombra.\n",// 3 giorni
+"A sofferir tormenti, caldi e geli\n simili corpi la Virtu' dispone\n che, come fa, non vuol ch'a noi si sveli.\n",
+"Matto e' chi spera che nostra ragione\n posssa trascorrer la infinita via\n che tiene una sustanza in tre persone.\n",
+"State contenti, umana gente, al quia;\n che, se potuto aveste veder tutto,\n mestier non era parturir Maria;\n",
+"e disiar vedeste sanza frutto\n tai che sarebbe lor didio quetato,\n ch'etternalmente e' dato lor per lutto:\n",
+"io dico d'Aristotile e di Plato\n e di molt'altri\"; e qui chino' la fronte,\n e piu' non disse, e rimase turbato.\n", // 4 giorni
+"Noi divenimmo intento al pie' del monte\n quivi trovammo la roccia si erta,\n che 'ndarno vi sarien la gambe pronte.\n",
+"Tra Lerice e Turbia la piu' diserta,\n la piu' rotta ruina e' una la scala,\n verso di quella, agevole e aperta.\n",
+"\"Or chi sa da qual man la costa cala\",\n disse 'l maestro mio fermando 'l passo,\n \"si che possa salir chi va sanz'ala?\".\n",
+"E mentre ch'e tenendo il viso basso\n essaminava del cammin la mente,\n e io mirava suso intorno al sasso,\n",
+"da man sinistra m'appari' una gente\n d'anime, che movieno i pie' ver' noi,\n e non pareva, si venian lente.\n",
+"\"Leva\", diss'io, \"maestro, li occhi tuoi\n ecco di qua chi ne dara' consiglio\n se tu da te medesmo aver nol puoi\".\n", // 5 giorni
+"Guardo' allora, e con libero piglio\n rispuose: \"Andiamo in la', ch'ei vegnon piano,\n e tu ferma la spene, dolce figlio\".\n",
+"Ancora era quel popol di lontano,\n i' dico dopo i nostri mille passi,\n quanto un buon gittator trarria con mano,\n",
+"quando si strinser tutti ai duri massi,\n de l'alta ripa, e stetter fermi e stretti,\n com' a guardar chi va, dubbiando stassi.\,", // 6 giorno
+"\"O ben finiti, o gia' spiriti eletti\",\n Virgilio incomincio',\"per quella pace\n ch' i credo che per voi tutti s'aspetti,\n",
+"ditene dove la monatgna giace, \n si' che possibil sia l'andare in suso;\n che' perder tempo a chi piu' sa piu' spiace\".\n", // 7 giorni
+"Come le pecorelle escon dal chiuso\n a una, a due, a tre, e l'altre stanno\n timidette atterrando l'occhio e 'l muso;\n",
+"e cio' che fa la prima, a l'altre fanno,\n addossandosi a lei, s'ella s'arresta,\n semplici e quete, e lo 'mperche' non sanno;\n",
+"si' vid'io muovere a venir la testa\n quella mandra fortunata alotta,\n pudica in faccia e nell'andare onesta.\n",
+"Come color dinanzi vider rotta\n la luce in terra dal mio destro canto,\n si' che l'ombra era da me a la grotta,\n", // 8 giorni
+"restaro, e trasser se' in dietro alquanto,\n e tutti li altri che venieno appresso,\n non sappiendo l' perche', fenno altrettanto.\n",
+"\"Sanza vostra domanda io vi confesso\n che questo e' corpo uman che voi vedete;\n per che 'l lume del sole in terra e' fesso.\n"
+],
+
+[
+"\"PER ME SI VA NE LA CITTA' DOLENTE, \nPER ME SI VA NE L'ETTERNO DOLORE,\n PER ME SI VA TRA LA PERDUTA GENTE.\n",
+"GIUSTIZIA MOSSE IL MIO ALTO FATTORE:\nFECEMI LA DIVINA PODESTATE,\nLA SOMMA SAPIENZA E 'L PRIMO AMORE.\n",
+"DINANZI A ME NON FUOR COSE CREATE\nSE NON ETTERNE, E IO ETTERNO DURO.\nLASCIATE OGNE SPERANZA, VOI CH'INTRATE\".\n",
+"Queste parole di colore oscuro\nvid'io scritte al sommo d'una porta;\nper ch'io: \"Maestro, il senso lor m'e' duro\".\n",
+"Ed elli a me, come persona accorta:\n\"Qui si convien lasciare ogne sospetto;\nogne vilta' convien che qui sia morta.\n",
+"Noi siam venuti al loco ov'i' t'ho detto\nche tu vedrai le genti dolorose\nc'hanno perduto il ben de l'intelletto\".\n",
+"E poi che la sua mano a la mia puose\ncon lieto volto, ond'io mi confortai,\nmi mise dentro a le segrete cose.\n",
+"Quivi sospiri, pianti e alti guai\nrisonavan per l'aere sanza stelle,\nper ch'io al cominciar ne lagrimai.\n",
+"Diverse lingue, orribili favelle,\nparole di dolore, accenti d'ira,\nvoci alte e fioche, e suon di man con elle\n",
+"facevano un tumulto, il qual s'aggira\nsempre in quell'aura sanza tempo tinta,\ncome la rena quando turbo spira.\n",
+"E io ch'avea d'error la testa cinta,\ndissi: \"Maestro, che e' quel ch'i' odo?\ne che gent'e' che par nel duol si' vinta?\".\n",
+"Ed elli a me: \"Questo misero modo\ntegnon l'anime triste di coloro\nche visser sanza 'nfamia e sanza lodo.\n",
+"Mischiate sono a quel cattivo coro\nde li angeli che non furon ribelli\nne' fur fedeli a Dio, ma per se' fuoro.\n",
+"Caccianli i ciel per non esser men belli,\nne' lo profondo inferno li riceve,\nch'alcuna gloria i rei avrebber d'elli\".\n",
+"E io: \"Maestro, che e' tanto greve\na lor, che lamentar li fa si' forte?\".\nRispuose: \"Dicerolti molto breve.\n",
+"Questi non hanno speranza di morte\ne la lor cieca vita e' tanto bassa,\nche 'nvidiosi son d'ogne altra sorte.\n",
+"Fama di loro il mondo esser non lassa;\nmisericordia e giustizia li sdegna:\nnon ragioniam di lor, ma guarda e passa\".\n",
+"E io, che riguardai, vidi una 'nsegna\nche girando correva tanto ratta,\nche d'ogne posa mi parea indegna;\n",
+"e dietro le veni'a si' lunga tratta\ndi gente, ch'i' non averei creduto\nche morte tanta n'avesse disfatta.\n",
+"Poscia ch'io v'ebbi alcun riconosciuto,\nvidi e conobbi l'ombra di colui\nche fece per viltade il gran rifiuto.\n",
+"Incontanente intesi e certo fui\nche questa era la setta d'i cattivi,\na Dio spiacenti e a' nemici sui.\n",
+"Questi sciaurati, che mai non fur vivi,\nerano ignudi e stimolati molto\nda mosconi e da vespe ch'eran ivi.\n",
+"Elle rigavan lor di sangue il volto,\nche, mischiato di lagrime, a' lor piedi\nda fastidiosi vermi era ricolto.\n",
+"E poi ch'a riguardar oltre mi diedi,\nvidi genti a la riva d'un gran fiume;\nper ch'io dissi: \"Maestro, or mi concedi\n",
+"ch'i' sappia quali sono, e qual costume\nle fa di trapassar parer si' pronte,\ncom'io discerno per lo fioco lume\".\n",
+"Ed elli a me: \"Le cose ti fier conte\nquando noi fermerem li nostri passi\nsu la trista riviera d'Acheronte\".\n",
+"Allor con li occhi vergognosi e bassi,\ntemendo no 'l mio dir li fosse grave,\ninfino al fiume del parlar mi trassi.\n",
+"Ed ecco verso noi venir per nave\nun vecchio, bianco per antico pelo,\ngridando: \"Guai a voi, anime prave!\n",
+"Non isperate mai veder lo cielo:\ni' vegno per menarvi a l'altra riva\nne le tenebre etterne, in caldo e 'n gelo.\n",
+"E tu che se' costi', anima viva,\npartiti da cotesti che son morti\".\nMa poi che vide ch'io non mi partiva,\n",
+"disse: \"Per altra via, per altri porti\nverrai a piaggia, non qui, per passare:\npiu' lieve legno convien che ti porti\".\n",
+"E 'l duca lui: \"Caron, non ti crucciare:\nvuolsi cosi' cola' dove si puote\ncio' che si vuole, e piu' non dimandare\".\n",
+"Quinci fuor quete le lanose gote\nal nocchier de la livida palude,\nche 'ntorno a li occhi avea di fiamme rote.\n",
+"Ma quell'anime, ch'eran lasse e nude,\ncangiar colore e dibattero i denti,\nratto che 'nteser le parole crude.\n",
+"Bestemmiavano Dio e lor parenti,\nl'umana spezie e 'l loco e 'l tempo e 'l seme\ndi lor semenza e di lor nascimenti.\n",
+"Poi si ritrasser tutte quante insieme,\nforte piangendo, a la riva malvagia\nch'attende ciascun uom che Dio non teme.\n",
+"Caron dimonio, con occhi di bragia,\nloro accennando, tutte le raccoglie;\nbatte col remo qualunque s'adagia.\n",
+"Come d'autunno si levan le foglie\nl'una appresso de l'altra, fin che 'l ramo\nvede a la terra tutte le sue spoglie,\n",
+"similemente il mal seme d'Adamo\ngittansi di quel lito ad una ad una,\nper cenni come augel per suo richiamo.\n",
+"Cosi' sen vanno su per l'onda bruna,\ne avanti che sien di la' discese,\nanche di qua nuova schiera s'auna.\n",
+"\"Figliuol mio\", disse 'l maestro cortese,\n\"quelli che muoion ne l'ira di Dio\ntutti convegnon qui d'ogne paese:\n",
+"e pronti sono a trapassar lo rio,\nche' la divina giustizia li sprona,\nsi' che la tema si volve in disio.\n",
+"Quinci non passa mai anima buona;\ne pero', se Caron di te si lagna,\nben puoi sapere omai che 'l suo dir suona\".\n",
+"Finito questo, la buia campagna\ntremo' si' forte, che de lo spavento\nla mente di sudore ancor mi bagna.\n",
+"La terra lagrimosa diede vento,\nche baleno' una luce vermiglia\nla qual mi vinse ciascun sentimento;\ne caddi come l'uom cui sonno piglia. \n"
+]
+];
